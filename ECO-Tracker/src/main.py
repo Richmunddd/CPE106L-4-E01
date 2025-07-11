ba3af8726959787d281963e91786afa5216579b5
Richmond <richmond.mantaring@gmail.com>
2025-07-11 23:54:49 +0800

    .

--- ECO-Tracker/src/main.py
+++ ECO-Tracker/src/main.py
@@ -1,6 +1,6 @@
 import flet as ft
 
-class EcoActionApp(ft.Control):  # Use UserControl instead of Control
+class EcoActionApp(ft.Control):  # Use Control instead of UserControl
     def __init__(self):
         super().__init__()
         self.points = 0
