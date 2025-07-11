bc422aac221cc3e7a1ea94629c40296e25ac63ff
Richmond <richmond.mantaring@gmail.com>
2025-07-11 22:53:45 +0800

    test

--- ECO-Tracker/src/main.py
+++ ECO-Tracker/src/main.py
@@ -1,1 +1,9 @@
+import flet as ft
+
+def main(page):
+    # Add some text to the page
+    page.add(ft.Text("Hello, Web Flet!"))
+
+# Start the app for web browser
+ft.app(target=main, view=ft.WEB_BROWSER)
 
